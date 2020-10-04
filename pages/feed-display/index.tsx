a7f2838ea4981781340f4c7d3cc3880ccd305ddf
İzer <izerozlu@gmail.com>
2020-10-04 20:11:42 +0300

    # Comments added for FeedDisplay page.

--- pages/feed-display/index.tsx
+++ pages/feed-display/index.tsx
@@ -24,16 +24,22 @@ const FeedDisplay: NextPage<
     IFeedDisplay.IProps,
     IFeedDisplay.InitialProps
 > = () => {
-    const feedDisplay = useSelector((state: IStore) => state.feedDisplay);
+    const feedDisplayStore = useSelector((state: IStore) => state.feedDisplay);
     const dispatch = useDispatch();
 
+    /**
+     * Filters out the given <code>feedList</code> with filtering parameters <code>releaseYear</code>,
+     * <code>programType</code> and -if current- <code>query</code>.
+     * @param feedList List of <code>Feed</code> to be filtered out.
+     * @returns Filtered list.
+     */
     const filterFeed = (feedList: Feed[]): Feed[] => {
-        const { query } = feedDisplay;
+        const { query } = feedDisplayStore;
         const queryTestRegExp = new RegExp(query, "i");
         const filteredFeedList = feedList.filter((feed: Feed) => {
             return (
                 feed.releaseYear > 2010 &&
-                feedDisplay.feedType.includes(feed.programType) &&
+                feedDisplayStore.feedType.includes(feed.programType) &&
                 (query ? queryTestRegExp.test(feed.title) : true)
             );
         });
@@ -41,10 +47,31 @@ const FeedDisplay: NextPage<
         return filteredFeedList.slice(0, 21);
     };
 
+    /**
+     * Sorts the given <code>feedList</code> with instruction from the given <code>sortType</code.
+     *
+     * <code>sortType</code> variations:
+     * <ul>
+     *  <li>"title_asc": Ascending order for the <code>title</code> property.</li>
+     *  <li>"title_desc": Descending order for the <code>title</code> property.</li>
+     *  <li>"years_asc": Ascending order for the <code>releaseYear</code> property.</li>
+     *  <li>"years_desc": Descending order for the <code>releaseYear</code> property.</li>
+     * </ul>
+     *
+     * @param feedList List of <code>Feed</code> to sort.
+     * @param sortType Type of sorting will be applied.
+     * @returns Sorted out <code>Feed</code> list.
+     */
     const sortFeed = (
         feedList: Feed[],
         sortType: SortType = "title_asc"
     ): Feed[] => {
+        /**
+         * Used to decide which feed will be prioritized to insert.
+         * @param feedA First <code>Feed</code> to compare.
+         * @param feedB Second <code>Feed</code> to compare.
+         * @returns Decision of  whether the first <code>Feed</code> has to be inserted before.
+         */
         const higherSortComparison = (feedA: Feed, feedB: Feed): boolean => {
             switch (sortType) {
                 case "title_asc": {
@@ -64,6 +91,12 @@ const FeedDisplay: NextPage<
                 }
             }
         };
+        /**
+         * Used to decide which feed will be prioritized to insert.
+         * @param feedA First <code>Feed</code> to compare.
+         * @param feedB Second <code>Feed</code> to compare.
+         * @returns Decision of whether the first <code>Feed</code> has to be inserted after.
+         */
         const lowerSortComparison = (feedA: Feed, feedB: Feed): boolean => {
             switch (sortType) {
                 case "title_asc": {
@@ -95,19 +128,30 @@ const FeedDisplay: NextPage<
         });
     };
 
-    const processFeed = async () => {
-        const { feedList } = feedDisplay;
-        const sortedList = sortFeed(feedList, feedDisplay.sortType);
+    /**
+     * Sorts and filters the <code>feedList</code> from the component's store.
+     */
+    const processFeed = async (): Promise<void> => {
+        const { feedList } = feedDisplayStore;
+        const sortedList = sortFeed(feedList, feedDisplayStore.sortType);
         const filteredList = filterFeed(sortedList);
         dispatch(FeedDisplayActions.AssignFilteredFeedList(filteredList));
     };
 
+    /**
+     * Decides which content will be rendered for the component.
+     * If the component is in a <i>loading</i> or <i>error</i> state, displays
+     * a simple message.
+     *
+     * If not returns the feed elements.
+     * @returns Content to be displayed.
+     */
     const renderContent = (): JSX.Element => {
-        if (feedDisplay.hasError || feedDisplay.loading) {
+        if (feedDisplayStore.hasError || feedDisplayStore.loading) {
             return (
                 <InnerContainer>
                     <AlternativeMessage>
-                        {feedDisplay.hasError
+                        {feedDisplayStore.hasError
                             ? "Oops, something went wrong.."
                             : "Loading..."}
                     </AlternativeMessage>
@@ -122,12 +166,14 @@ const FeedDisplay: NextPage<
         );
     };
 
+    // This useEffect hook is used for initial setup of the component.
     useEffect(() => {
         fetchFeed(dispatch);
     }, []);
 
+    // This useEffect hook is used for state update digestions.
     useEffect(() => {
-        if (feedDisplay.feedNeedsProcessing) {
+        if (feedDisplayStore.feedNeedsProcessing) {
             processFeed();
         }
     });
@@ -138,7 +184,9 @@ const FeedDisplay: NextPage<
             <Content>
                 <Header>
                     <InnerContainer>
-                        <Subheader>popular {feedDisplay.feedType}</Subheader>
+                        <Subheader>
+                            popular {feedDisplayStore.feedType}
+                        </Subheader>
                     </InnerContainer>
                 </Header>
                 {renderContent()}
@@ -158,7 +206,12 @@ FeedDisplay.getInitialProps = async (
     return {};
 };
 
-const fetchFeed = async (dispatch: Dispatch) => {
+/**
+ * Fetches a list of feed from the server. If this retrieval is successful,
+ * saves the to the store which's <code>dispatch</code> method has been given as a parameter.
+ * @param dispatch Method to notify a store about the feed list retriaval.
+ */
+const fetchFeed = async (dispatch: Dispatch): Promise<void> => {
     try {
         const result = await Http.Request<FeedResponse>("GET", ENDPOINTS.feed);
         dispatch(FeedDisplayActions.AssignFeedList(result.entries));
